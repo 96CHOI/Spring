4cf3054bfc5f7dffa5746e78c0d0ff9ebb6b25e4
96CHOI <cyd684@gmail.com>
2022-11-30 15:17:29 +0900

    오후 Spring 수업

---
+++ src/main/java/com/example/demo/mapper/EmpMapper.java
@@ -0,0 +1,41 @@
+package com.example.demo.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.example.demo.vo.DeptVO;
+import com.example.demo.vo.EmpVo;
+
+/*
+ * 추상클래스와 인터페이스 차이점.
+ * 인터페이스 : 메소드 정의
+ * 추상클래스 : 정의, 구현 가능 
+ */
+
+
+@Mapper
+public interface EmpMapper {
+	
+	/**
+	 * @Since : 2022. 11. 28.
+	 * @Author : mr.Choi
+	 * @Return : List<EmpVo>
+	 * @Comment : EMP 테이블 전체 조회
+	 */
+	// art + shift + J
+	List<EmpVo> selectEmp(); //메소드 정의
+	
+	/**
+	 * @Since : 2022. 11. 29.
+	 * @Author : mr.Choi
+	 * @Return : List<DeptVO>
+	 * @Comment : 
+	 */
+	List<DeptVO> selectDept();
+
+	int insertEMP(EmpVo vo);
+	
+	
+	
+}
